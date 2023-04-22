7b1dc1dd95b8da500064c8aceb0a950d2188e306
Lakshay Bhardwaj <91718893+lakshay1121@users.noreply.github.com>
2023-04-22 21:30:52 +0530

    Add files via upload

---
+++ Screens/Home.js
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+import * as StoreReview from "expo-store-review";
+
+const handleReview = () => {
+
+  if(StoreReview.hasAction()){
+
+    StoreReview.requestReview();
+  }
+}
+
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+
+
+      <TouchableOpacity onPress={handleReview()} style={styles.button}>
+        <Text style={styles.buttonText}>review</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+
+  button:{
+   
+    width:200,
+    height:40,
+    backgroundColor:'blue',
+    display:'flex',
+    alignItems:'center',
+    borderRadius:25
+
+  },
+
+  buttonText:{
+    color:'white',
+    fontSize:22
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
